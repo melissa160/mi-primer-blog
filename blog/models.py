5b64c32d208f89a441e402cd60c08eec2d688511
melissa160 <melissa160@gmail.com>
2017-02-12 16:35:02 -0500

    usando el modelo dentro de la vista

--- blog/models.py
+++ blog/models.py
@@ -3,19 +3,19 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    #atributos del modelo Post
-    author = models.ForeignKey('auth.User')#este campo se relaciona con la clase auth
-    title = models.CharField(max_length=300)#campo tipo char
-    text = models.TextField()#campo de texto    
+    # atributos del modelo Post
+    author = models.ForeignKey('auth.User')  # este campo se relaciona con la clase auth
+    title = models.CharField(max_length=300)  # campo tipo char
+    text = models.TextField()  # campo de texto
     created_date = models.DateTimeField(
-            default=timezone.now)#pone la fecha actual
-    published_date = models.DateTimeField(#requiere que el usuario digite la fecha pero puede ser blanco o nulo
+            default=timezone.now)  # pone la fecha actual
+    published_date = models.DateTimeField(  # requiere que el usuario digite la fecha pero puede ser blanco o nulo
             blank=True, null=True)
 
-    #metodo del modelo Post
-    def publish(self):#el self es explicito y se pone en los metodos de una clase (Post)
-        self.published_date = timezone.now()#para el post que estoy llamando con published le coloca la fecha actual
-        self.save()#guarda el self que es la instancia de la clase
+    # metodo del modelo Post
+    def publish(self):  # el self es explicito y se pone en los metodos de una clase (Post)
+        self.published_date = timezone.now()  # para el post que estoy llamando con published le coloca la fecha actual
+        self.save()  # guarda el self que es la instancia de la clase
 
-    def __str__(self):#definicion de la forma como quiero ver el post cuando lo imprima
+    def __str__(self):  # definicion de la forma como quiero ver el post cuando lo imprima
         return self.title
